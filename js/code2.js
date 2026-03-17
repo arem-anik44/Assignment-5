ef6db822d38333835934b8dd5777da7a88b6692f
arem-anik44 <arem.anik8044@gmail.com>
2026-03-18 00:55:13 +0600

    make search option function and number change in issue count

--- js/code2.js
+++ js/code2.js
@@ -12,6 +12,12 @@ const loadContainer=(st='all')=>{
         
     });
 };
+
+const createElements = (arr) => {
+  const htmlElements = arr.map((el) => `<h4 class="btn bg-[#FDE68A] rounded-2xl text-[#D97706]">${el}</h4>`);
+  return htmlElements.join(" ");
+};
+
 const removeActive = () => {
   const lessonButtons = document.querySelectorAll(".click-btn");
   
@@ -67,14 +73,13 @@ const displayContainer=(cards,st)=>{
                     <h4 class="btn bg-[#FEECEC] rounded-2xl text-red-500 text-[12px]">${card.priority.toUpperCase()}</h4>
                 </div>
                 <div class="px-4 text-left space-y-3 flex-1 flex flex-col">
-                    <div class="flex-1">
+                    <div class="flex-1 space-y-3">
                         <h2 class="font-semibold  ">${card.title}</h2>
                     <p class="text-[14px] text-[#64748B]">${card.description}</p>
                     </div>
                     
-                    <div id="labels" class="flex items-center gap-2 h-14 ">
-                        <h4 class="btn bg-[#FDE68A] rounded-2xl text-[#D97706]">bug</h4>
-                        <h4 class="btn bg-[#FDE68A] rounded-2xl text-[#D97706]">help Wanted</h4>
+                    <div id="labels" class="flex items-center gap-2 h-14 flex-wrap  ">
+                         ${createElements(card.labels)}
                     </div>
                 </div>
 
@@ -121,14 +126,13 @@ const displayContainerSpecific=(cards,st)=>{
                     <h4 class="btn bg-[#FEECEC] rounded-2xl text-red-500 text-[12px]">${card.priority.toUpperCase()}</h4>
                 </div>
                 <div class="px-4 text-left space-y-3 flex-1 flex flex-col">
-                    <div class="flex-1">
+                    <div class="flex-1 space-y-3">
                         <h2 class="font-semibold  ">${card.title}</h2>
                     <p class="text-[14px] text-[#64748B]">${card.description}</p>
                     </div>
                     
                     <div id="labels" class="flex items-center gap-2 h-14 ">
-                        <h4 class="btn bg-[#FDE68A] rounded-2xl text-[#D97706]">bug</h4>
-                        <h4 class="btn bg-[#FDE68A] rounded-2xl text-[#D97706]">help Wanted</h4>
+                        ${createElements(card.labels)}
                     </div>
                 </div>
 
@@ -175,4 +179,66 @@ const showClosed=()=>{
     removeActive();
     document.getElementById("closed-btn").classList.add("active");
     loadContainer("closed");
+};
+
+
+
+document.getElementById("btn-search").addEventListener("click", () => {
+  removeActive();
+  const input = document.getElementById("input-search");
+  const searchValue = input.value.trim().toLowerCase();
+  console.log(searchValue);
+
+  url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      
+
+      displayLevelWord(data.data);
+    });
+});
+
+
+const displayLevelWord=(cards)=>{
+    
+    const cardsContainer = document.getElementById("cards-container");
+    cardsContainer.innerHTML ="";
+    cards.forEach(card => {
+
+     const cardHolder= document.createElement("div");
+        cardHolder.innerHTML = `
+        
+            <button class=" shadow-lg space-y-4 rounded pt-5 h-full flex flex-col ">
+                <div class="flex justify-between items-center px-6 h-[40px]">
+                    ${card.status === 'open'
+                        ? `<img src="./assets/Open-Status.png" alt="">`
+                        : `<img src="./assets/Closed- Status .png" alt="">`
+                    }
+                    <h4 class="btn bg-[#FEECEC] rounded-2xl text-red-500 text-[12px]">${card.priority.toUpperCase()}</h4>
+                </div>
+                <div class="px-4 text-left space-y-3 flex-1 flex flex-col">
+                    <div class="flex-1 space-y-3">
+                        <h2 class="font-semibold  ">${card.title}</h2>
+                    <p class="text-[14px] text-[#64748B]">${card.description}</p>
+                    </div>
+                    
+                    <div id="labels" class="flex items-center gap-2 h-14 ">
+                        ${createElements(card.labels)}
+                    </div>
+                </div>
+
+                <div class="text-left text-[#64748B] px-5 border-t border-gray-300 h-[80px]">
+                    <p class="pt-2">#1 by ${(card.author) ? card.author: "author_not_found"}</p>
+                    <p><p>${new Date(card.createdAt).toLocaleDateString()}</p></p>
+                </div>
+            </button>
+        
+        
+        `;
+
+        cardsContainer.append(cardHolder);
+
+    });
 };
