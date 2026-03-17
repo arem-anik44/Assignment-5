f33e198e268f1eca403b39c3add70827e7075d89
arem-anik44 <arem.anik8044@gmail.com>
2026-03-17 10:46:51 +0600

    login page complete working properly

--- js/code.js
+++ js/code.js
@@ -1,1 +1,21 @@
-console.log("its working");
+document.getElementById("signin-btn").addEventListener("click",()=>{
+    const username = document.getElementById("username-input").value;
+    
+    const password = document.getElementById("pass-input").value;
+    
+
+    if(username === 'admin' & password==='password'){
+        window.location.assign("./home.html");
+    }
+    else{
+        if(username !== 'admin' & password !=='password'){
+            alert("Invalid username and password");
+        }
+        else if(username!== 'admin'){
+            alert("Invalid User");
+        }
+        else{
+            alert("Invalid password");
+        }
+    }
+})
